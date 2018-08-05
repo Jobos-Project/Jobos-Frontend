67cab4bd74fa7cb0671ae52c22b21c5d70c29bde
Zubogain <zubogain@gmail.com>
2018-08-05 13:51:56 +0600

    0.1.0 - Skeleton

---
+++ src/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <main id="main" class="col-12">
+        <nav class="navbar navbar-expand navbar-dark bg-custom-green row">
+            <a id="user-greeting" class="col-4 offset-2">Jobos</a>
+        </nav>
+        <div class="col-8 offset-2">
+            <transition name="fade" mode="out-in" appear>
+                <router-view class="col-12"></router-view>
+            </transition>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    }
+</script>
