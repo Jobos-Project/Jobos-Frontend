f5a69230335bc5a71d8245a7a70bee0f73885c78
Zubogain <zubogain@gmail.com>
2018-08-09 18:25:10 +0600

    0.4.1 Babel preset testing

--- src/js/store/modules/Jobs.js
+++ src/js/store/modules/Jobs.js
@@ -10,7 +10,7 @@ const getters = {
 
 const actions = {
     getJobs({ commit }) {
-        axios.get("http://78.140.221.46/api/show_jobs/?format=json").then(response => {
+        axios.get("http://78.140.221.46:7777/api/show_jobs/?format=json").then(response => {
             commit('setJobs', response.data);
         }).catch(error => {
             console.log(error)
